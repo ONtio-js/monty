c6b55cc63981e3b97408c56e027ac0914cf20c0e
SylvanusNK <107428803+SylvanusNK@users.noreply.github.com>
2022-10-07 04:49:38 +0100

    Update addnode.c

--- addnode.c
+++ addnode.c
@@ -1,6 +1,6 @@
 #include "monty.h"
 /**
- * addnode - add node to the head stack
+ * addnode - function that add node to the head stack
  * @head: head of the stack
  * @n: new_value
  * Return: no return
