06ada3ffabf7e27e4c6fd4147f853884c85a6d3c
SylvanusNK <107428803+SylvanusNK@users.noreply.github.com>
2022-10-07 04:46:12 +0100

    Update execute.c

--- execute.c
+++ execute.c
@@ -1,6 +1,6 @@
 #include "monty.h"
 /**
-* execute - executes the opcode
+* execute - a function that executes the opcode
 * @stack: head linked list - stack
 * @counter: line_counter
 * @file: poiner to monty file
