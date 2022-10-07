da18f954ee4167b78bf13a6daf0e2ced27a43a49
SylvanusNK <107428803+SylvanusNK@users.noreply.github.com>
2022-10-07 04:45:18 +0100

    Update add.c

--- add.c
+++ add.c
@@ -1,6 +1,6 @@
 #include "monty.h"
 /**
- * f_add - adds the top two elements of the stack.
+ * f_add - a function that adds the top two elements of the stack.
  * @head: stack head
  * @counter: line_number
  * Return: no return
